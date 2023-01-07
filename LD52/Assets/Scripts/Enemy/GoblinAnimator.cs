b5a57af8899e527980c9876aefc8c32f5f87aee1
Juutis <>
2023-01-07 17:10:27 +0200

    Added goblin animations

---
+++ LD52/Assets/Scripts/Enemy/GoblinAnimator.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoblinAnimator : MonoBehaviour
+{
+    private Animator anim;
+
+    [SerializeField]
+    private bool debugShoot;
+
+    [SerializeField]
+    private bool debugWalk;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (debugWalk) {
+            SetWalking(!anim.GetBool("Walk"));
+            debugWalk = false;
+        }
+        if (debugShoot) {
+            Shoot();
+            debugShoot = false;
+        }
+    }
+
+    public void SetWalking(bool walking)
+    {
+        anim.SetBool("Walk", walking);
+    }
+
+    public void Shoot()
+    {
+        anim.SetBool("Shoot", true);
+    }
+
+    public void ShootCallback()
+    {
+        anim.SetBool("Shoot", false);
+    }
+}
