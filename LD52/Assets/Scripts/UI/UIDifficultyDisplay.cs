943a0617029df73e86a6d770e41a17ac790d7535
Juho Saarelainen <bradurdung@gmail.com>
2023-01-08 16:31:32 +0200

    Display font & color palette

--- LD52/Assets/Scripts/UI/UIDifficultyDisplay.cs
+++ LD52/Assets/Scripts/UI/UIDifficultyDisplay.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 public class UIDifficultyDisplay : MonoBehaviour
@@ -8,8 +9,11 @@ public class UIDifficultyDisplay : MonoBehaviour
 
     [SerializeField]
     private TextMeshProUGUI txtDifficulty;
+    [SerializeField]
+    private Image imgDifficulty;
     public void Initialize(Difficulty difficulty)
     {
         txtDifficulty.text = $"Difficulty: {difficulty.Type}";
+        imgDifficulty.color = difficulty.Color;
     }
 }
