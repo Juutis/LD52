d4dd947b89a111ab949ee2a0e114af930f3f078b
Matias Pitkänen <M2tias@Gmail.com>
2023-01-08 16:08:38 +0200

    Force pull berries

---
+++ LD52/Assets/Scripts/SpellCasting/PullActions/ForcePullGather.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForcePullGather : MonoBehaviour
+{
+
+    [SerializeField]
+    private List<Transform> berries = new List<Transform>();
+    [SerializeField]
+    private int pullBerryCount;
+    [SerializeField]
+    private float pullSpeed;
+
+    private List<Transform> allBerries;
+    private List<Transform> berriesBeingPulled = new List<Transform>();
+    private List<Vector3> startPositions = new List<Vector3>();
+    private Vector3 targetPosition;
+    private float lerpAmount = 0;
+    private bool pulling = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        allBerries = new List<Transform>(berries);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (pulling)
+        {
+            targetPosition = PlayerMovement.main.transform.position;
+            lerpAmount += pullSpeed * Time.deltaTime;
+
+            for (int i = 0; i < berriesBeingPulled.Count; i++)
+            {
+                var berry = berriesBeingPulled[i];
+                var startPos = startPositions[i];
+                berry.position = Vector3.Lerp(startPos, targetPosition, lerpAmount);
+            }
+
+            if (lerpAmount >= 1)
+            {
+                pulling = false;
+                for (int i = 0; i < berriesBeingPulled.Count; i++)
+                {
+                    var berry = berriesBeingPulled[i];
+                    Destroy(berry.gameObject);
+                }
+
+                berriesBeingPulled = new List<Transform>();
+                startPositions = new List<Vector3>();
+            }
+        }
+    }
+
+    public void PullAction()
+    {
+        for (int i = 0; i < pullBerryCount; i++)
+        {
+            if (allBerries.Count > 1)
+            {
+                Transform berry = allBerries[0];
+                allBerries.Remove(berry);
+                berriesBeingPulled.Add(berry);
+                startPositions.Add(berry.position);
+                pulling = true;
+                lerpAmount = 0;
+            }
+        }
+    }
+}
