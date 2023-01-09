da061a970fecfa2092131ebc18d7973053889d9a
Juutis <>
2023-01-09 12:33:44 +0200

    Level stuffs

--- LD52/Assets/Scripts/RandomYaw.cs
+++ LD52/Assets/Scripts/RandomYaw.cs
@@ -7,7 +7,9 @@ public class RandomYaw : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        var scale = Random.Range(0.9f, 1.1f);
         transform.Rotate(Vector3.up, Random.Range(0, 360));
+        transform.localScale = new Vector3(scale, scale, scale);
         Destroy(this);
     }
 
