34be8785fd931f760bd08b3af5c878fa569ae863
Juutis <>
2023-01-09 14:40:36 +0200

    Plum stuffs

---
+++ LD52/Assets/Scripts/SpellCasting/PullActions/ForcePullPlum.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForcePullPlum : MonoBehaviour
+{
+    private static float unscaledAnimationDuration = 2.042f;
+    private static float steps = 5;
+    private static float durationPerStep = 1.0f;
+    private static float animationScale = unscaledAnimationDuration / (steps * durationPerStep);
+
+    private bool pulling = false;
+
+    private Animator anim;
+    private int curStep = 0;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponentInChildren<Animator>();
+        anim.speed = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (pulling)
+        {
+
+        }
+    }
+
+    public void PullAction()
+    {
+        if (!pulling) 
+        {
+            if (curStep < steps)
+            {
+                pulling = true;
+                Invoke("StopAnimation", durationPerStep);
+                anim.speed = animationScale;
+                curStep++;
+            }
+            else
+            {
+                Debug.Log("WIN!");
+                GameManager.main.TheEnd();
+            }
+        }
+    }
+
+    public void StopAnimation()
+    {
+        anim.speed = 0;
+        pulling = false;
+    }
+}
